8d44d11668dc92681994bfcf904ed5e8d53641f6
Katsuya Ishiyama <ishiyama.katsuya@gmail.com>
2018-01-05 02:57:24 +0900

    data IO was changed to fit GCP

--- vgg/trainer/data_loader.py
+++ vgg/trainer/data_loader.py
@@ -2,11 +2,12 @@
 
 import json
 import numpy as np
+from tensorflow.python.lib.io import file_io
 
 
 def data_loader(filepath):
 
-    with open(filepath) as f:
+    with file_io.FileIO(filepath, "r") as f:
         data = json.load(f)
 
     images = np.array(data['images'])
