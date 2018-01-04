22763c12c01b1a210fc7bdb679a61682cd06b877
Katsuya Ishiyama <ishiyama.katsuya@gmail.com>
2018-01-05 03:00:37 +0900

    modified to use for yuruchara data

--- vgg/job_submit.sh
+++ vgg/job_submit.sh
@@ -4,32 +4,19 @@ GCP_REGION="asia-east1"
 
 PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 
-UPLOAD_DATA_DIR="mnist"
+CHARACTER_TYPE="gotochi"
 
-BUCKET_NAME="vgg19"
-# BUCKET_NAME=${PROJECT_ID}-ml
+BUCKET_NAME="yuruchara"
 TRAIN_BUCKET=gs://$BUCKET_NAME
 
-# 既にバケットがある場合は削除する
-gsutil rm -rf $TRAIN_BUCKET
-
-# バケットを作成する
-gsutil mb -l $GCP_REGION $TRAIN_BUCKET
-
-# 画像データをpickleに変換する
-# python /home/ishiyama/vgg/pickle_mnist.py
-
-# 画像データをアップロードする
-gsutil cp -r $UPLOAD_DATA_DIR $TRAIN_BUCKET
-
 JOB_NAME=${BUCKET_NAME}_$(date +%Y%m%d%H%M%S)
-GCP_SOURCE_DATA_DIR=$TRAIN_BUCKET/${UPLOAD_DATA_DIR}
 gcloud ml-engine jobs submit training $JOB_NAME \
+    --config=job_config.yaml \
     --package-path=trainer \
-    --module-name=trainer.task \
+    --module-name=trainer.task\
     --staging-bucket=$TRAIN_BUCKET \
     --region=$GCP_REGION \
     -- \
     --train-steps 1000 \
-    --input-data-dir $GCP_SOURCE_DATA_DIR
+    --input-path $TRAIN_BUCKET/2017/${CHARACTER_TYPE}/${CHARACTER_TYPE}.json
 
